cd37ab317712a993744fe40e4d16d085d03b533d
tarunmanchukonda <tarun.manchu15@gmail.com>
2019-04-17 14:45:06 +0530

    styling in login and register page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -55,7 +55,7 @@ class Login extends Component {
       <div className="login">
         <div className="container">
           <div className="row">
-            <div className="col-md-8 m-auto">
+            <div className="col-md-5 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">
                 Log in to your UrbanSoal account
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -56,7 +56,7 @@ class Register extends Component {
       <div className="register">
         <div className="container">
           <div className="row">
-            <div className="col-md-8 m-auto">
+            <div className="col-md-5 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
               <p className="lead text-center">
                 Sign up to UrbanSoal account
